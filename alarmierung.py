29fe69593e1afb49d45fb7861594690a21f5d59d
Julian Brügger <49551737+julianbruegger@users.noreply.github.com>
2020-08-14 08:40:17 +0200

    Create alarmierung.py
    Script für die Alarmierung

---
+++ alarmierung.py
@@ -0,0 +1,18 @@
+
+# Download the helper library from https://www.twilio.com/docs/python/install
+from twilio.rest import Client
+
+
+# Your Account Sid and Auth Token from twilio.com/console
+# DANGER! This is insecure. See http://twil.io/secure
+account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+auth_token = 'your_auth_token'
+client = Client(account_sid, auth_token)
+
+call = client.calls.create(
+                        url='http://connect.julian-bruegger.tk/test.xml',
+                        to='+47765974891',
+                        from_='+15017122661'
+                    )
+
+print(call.sid)
