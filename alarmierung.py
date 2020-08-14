7f27ad1ba78355d830ed4d5ebbecc6517a0509b6
Julian Brügger <brj@ICT-BZ.ch>
2020-08-14 08:53:07 +0200

    if else

--- alarmierung.py
+++ alarmierung.py
@@ -1,18 +1,23 @@
 
 # Download the helper library from https://www.twilio.com/docs/python/install
-from twilio.rest import Client
+#from twilio.rest import Client
 
+wasserstand = '1.00'
 
+if wasserstand < '2.00':
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
-account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
-auth_token = 'your_auth_token'
-client = Client(account_sid, auth_token)
+    account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+    auth_token = 'your_auth_token'
+    client = Client(account_sid, auth_token)
 
-call = client.calls.create(
-                        url='http://connect.julian-bruegger.tk/test.xml',
-                        to='+47765974891',
-                        from_='+15017122661'
-                    )
+    call = client.calls.create(
+        url='http://connect.julian-bruegger.tk/test.xml',
+        to='+47765974891',
+        from_='+15017122661'
+        )
+    print ('alarm')
+    print(call.sid)
 
-print(call.sid)
+else:
+    print ('status i.o.')
